88150c120ecf3a1135e64472de2c2d3bb6d08d6f
Teeshan Dey <108420754+Teeshan10@users.noreply.github.com>
2026-08-20 03:14:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import json
 from groq import Groq
 
 # 1. SET UP THE WEB UI
-st.title("Teeshan's AI DEMO 🤖 ")
+st.title("TeeAI🤖 ")
 st.write("Ask me to find hotels, restaurants, or local attractions anywhere in the world!")
 
 # 2. LOAD SECRET KEYS (Hidden safely from the public)
